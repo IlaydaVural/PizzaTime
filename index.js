c0096dbf2665d30a15090193af3bce208f1beffb
IlaydaVural <53787895+IlaydaVural@users.noreply.github.com>
2019-08-13 12:00:49 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+function typeOfPizza(pizza){
+  var pizza = ["Margherita Pizza", "NYC Pizza", "Chicago Deep Dish Pizza", "Turkish Pizza"]
+  var random = Math.floor(Math.random()) * 4);
+  window.alert(pizza[random]);
+}
